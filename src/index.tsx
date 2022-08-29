b40207cd196255297287ecd98fcd5b146375355a
d954mas <d954mas@gmail.com>
2022-08-29 17:34:34 +0300

    connected to roketo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import { Buffer } from 'buffer';
+import {Buffer} from 'buffer';
 import {
     connect,
     keyStores,
     transactions,
     WalletConnection,
 } from 'near-api-js';
-import type { Action as NearAction } from 'near-api-js/lib/transaction';
-import { initApiControl } from '@roketo/sdk';
-import type { TransactionMediator } from '@roketo/sdk/dist/types';
+import type {Action as NearAction} from 'near-api-js/lib/transaction';
+import {initApiControl} from '@roketo/sdk';
+import type {TransactionMediator} from '@roketo/sdk/dist/types';
 
-
+global.Buffer = Buffer;
 const NEAR_CONSTANTS = {
     nodeUrl: 'https://rpc.testnet.near.org',
     walletUrl: 'https://wallet.testnet.near.org',
@@ -18,8 +18,6 @@ const NEAR_CONSTANTS = {
     financeContractName: 'finance-r-v2.dcversus.testnet',
     wNearContractName: 'wnear.testnet',
 };
-
-
 
 
 let keyStore = new keyStores.BrowserLocalStorageKeyStore();
@@ -32,43 +30,52 @@ declare const JsToDef: any;
 
 // Anonymous function
 let testLib = function () {
-  console.log("testLib")
+    console.log("testLib")
 };
 
 
-let initNear = function (){
-    if(!window.game_sdk.near){
+let initNear = function () {
+    if (!window.game_sdk.near) {
         connect({
             nodeUrl: NEAR_CONSTANTS.nodeUrl,
             walletUrl: NEAR_CONSTANTS.walletUrl,
             networkId: NEAR_CONSTANTS.networkId,
             keyStore,
             headers: {},
-        }).then(function (near){
-            window.game_sdk.near = near;
-            JsToDef.send("NearInitSuccess");
-            walletConnection = new WalletConnection(near, NEAR_CONSTANTS.roketoContractName);
-            account = walletConnection.account();
-            transactionMediator = {
-                functionCall: transactions.functionCall,
-                // @ts-expect-error signAndSendTransaction is protected
-                signAndSendTransaction: account.signAndSendTransaction,
-            };
-            JsToDef.send("NearInitWalletSuccess");
-
-        }).catch(function (error){
-            JsToDef.send("NearInitError",{error:error});
-            console.error("near connect error:" + error);
-        });
-    }else{
-        JsToDef.send("NearInitError",{error:"NearAlreadyInited"});
+        })
+            .then(function (near) {
+                window.game_sdk.near = near;
+                JsToDef.send("NearInitSuccess");
+                walletConnection = new WalletConnection(near, NEAR_CONSTANTS.roketoContractName);
+                account = walletConnection.account();
+                transactionMediator = {
+                    functionCall: transactions.functionCall,
+                    // @ts-expect-error signAndSendTransaction is protected
+                    signAndSendTransaction: account.signAndSendTransaction,
+                };
+                JsToDef.send("NearInitWalletSuccess");
+                return initApiControl({
+                    account,
+                    transactionMediator,
+                    roketoContractName: NEAR_CONSTANTS.roketoContractName,
+                });
+            })
+            .then(function (roketoApiControl) {
+                window.game_sdk.roketoApiControl = roketoApiControl;
+                JsToDef.send("NearInitRoketoApiControlSuccess");
+            })
+            .catch(function (error) {
+                JsToDef.send("NearError", {error: error});
+                console.error("near connect error:" + error);
+            });
+    } else {
+        JsToDef.send("NearError", {error: "NearAlreadyInited"});
         console.error("near already existed")
     }
 
 }
 
 window.game_sdk = {
-	buffer : Buffer,
-    initNear:initNear,
-	testLib:testLib,
+    initNear: initNear,
+    testLib: testLib,
 }
